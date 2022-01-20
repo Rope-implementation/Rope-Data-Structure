1815ca163e7e410e34f1da6664dc65fc712390b3
alireza <alirezasajjadi.asj@gmail.com>
2022-01-20 14:52:13 +0330

    creating rope

--- Rope.java
+++ Rope.java
@@ -2,11 +2,33 @@ public class Rope {
     private static final int MAX_LEAF_LEN = 6;
     Node root;
 
-    public Rope(){
+    public Rope() {
         root = new Node();
     }
 
-    void createNewRope(Node root, String input,int len) {
+    void createNewRope(Node parent, String input, int lIndex,int rIndex) {
+        Node p = new Node(input);
+        int leftLen = (int) Math.ceil(input.split(" ").length / 2.0);
+        int rightLen = input.length() - leftLen;
+        int[]x =spaceCharAt(input);
+        if(x.length==0){
+            parent = p;
+        }
+        else {
+            createNewRope(parent.left,input.substring(lIndex,x[leftLen]+1),lIndex,x[leftLen]);
+            createNewRope(parent.right,input.substring(x[leftLen]+1,rIndex),x[leftLen]+1,rIndex);
+        }
+
+    }
+
+
+    int[] spaceCharAt(String str) {
+        int j=0,x[]=new int[50];
+        for(int i=0;i<str.length();i++){
+            if (str.charAt(i)==' ')
+                x[j++] = i;
+        }
+        return x;
     }
 
 }
@@ -16,15 +38,15 @@ class Node {
     String str;
     int length;
 
-    public Node(){
-        left=null;
-        right=null;
-        str=null;
+    public Node() {
+        left = null;
+        right = null;
+        str = null;
     }
 
-    public Node(String str){
-        left=right=null;
-        length=str.length();
+    public Node(String str) {
+        left = right = null;
+        length = str.length();
     }
 
 }
