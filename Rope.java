6358f1c6e57fa00bb1eb7254558ab25dabebe4b2
alireza <alirezasajjadi.asj@gmail.com>
2022-01-20 11:27:26 +0330

    node class uptade

--- Rope.java
+++ Rope.java
@@ -1,11 +1,30 @@
 public class Rope {
     private static final int MAX_LEAF_LEN = 6;
-    
+    Node root;
+
+    public Rope(){
+        root = new Node();
+    }
+
+    void createNewRope(Node root, String input,int len) {
+    }
 
 }
 
-class Node{
-    Node left,right,parent;
+class Node {
+    Node left, right, parent;
     String str;
-    int length_leftWords;
+    int length;
+
+    public Node(){
+        left=null;
+        right=null;
+        str=null;
+    }
+
+    public Node(String str){
+        left=right=null;
+        length=str.length();
+    }
+
 }
