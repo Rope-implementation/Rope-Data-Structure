6f89a65dc419ed35b97fab3313e3d023600df6fd
alireza <alirezasajjadi.asj@gmail.com>
2022-01-21 20:02:27 +0330

    fixing bug

--- Rope.java
+++ Rope.java
@@ -11,28 +11,44 @@ public class Rope {
     }
 
     void createNewRope(Node node, String input, int lIndex, int rIndex) {
-        if(node!=root)
-            node = new Node();
+//        if(node==null)
+//            node = new Node();
 //        p.parent = parent_current;
 
         int leftLen = (int) Math.ceil(input.split(" ").length / 2.0);
         int rightLen = input.split(" ").length - leftLen;
         ArrayList<Integer> x = spaceCharAt(input);
 
-        if (x.size() == 1 || x.size() == 0) {
+        if (input.endsWith(" ")) {        //exception: do not have space in the end of first input
 //            node = p;
             node.str = input;
         } else {
 //            node = p;
             node.weight +=  input.substring(lIndex, x.get(leftLen - 1) + 1).length();
 //            parent_current.left = node;
+            if(node.left==null){
+                node.left = new Node();
+            }
             createNewRope(node.left, input.substring(lIndex, x.get(leftLen - 1) + 1), lIndex, x.get(leftLen - 1) + 1);
 //            parent_current.right=node;
+            if(node.right==null){
+                node.right = new Node();
+            }
             createNewRope(node.right, input.substring(x.get(leftLen - 1) + 1, rIndex), x.get(leftLen - 1) + 1, rIndex);
         }
 
-        System.out.println("saf");
+        printstring(root);
 
+    }
+
+    void printstring(Node root)
+    {
+        if (root==null)
+            return;
+        if (root.left==null && root.right==null)
+            System.out.println(root.str);
+        printstring(root.left);
+        printstring(root.right);
     }
 
 
