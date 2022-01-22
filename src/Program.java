4c51df73487c628e5603409677f22a77b17ae175
alireza <alirezasajjadi.asj@gmail.com>
2022-01-22 16:34:09 +0330

    program class, test

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,11 @@
-public class Program {
+import java.util.ArrayList;
 
+public class Program {
+    public static void main(String[] args) {
+        Rope r=new Rope("i am ali");
+        Rope r1= new Rope("i am rope DS");
+        ArrayList<Rope> rr =new ArrayList<>();
+        rr.add(r);rr.add(r1);
+        Rope.status(rr);
+    }
 }
